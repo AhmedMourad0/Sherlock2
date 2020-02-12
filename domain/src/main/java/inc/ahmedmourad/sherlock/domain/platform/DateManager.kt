f96d151fbb21309e7b13342d7173b025296beb3f
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-02-12 03:39:21 +0200

    Made unused constants private

--- domain/src/main/java/inc/ahmedmourad/sherlock/domain/platform/DateManager.kt
+++ domain/src/main/java/inc/ahmedmourad/sherlock/domain/platform/DateManager.kt
@@ -9,11 +9,11 @@ interface DateManager {
     ): String
 
     companion object {
-        const val SECOND_IN_MILLIS = 1000L
+        private const val SECOND_IN_MILLIS = 1000L
         const val MINUTE_IN_MILLIS = SECOND_IN_MILLIS * 60L
-        const val HOUR_IN_MILLIS = MINUTE_IN_MILLIS * 60L
-        const val DAY_IN_MILLIS = HOUR_IN_MILLIS * 24L
-        const val WEEK_IN_MILLIS = DAY_IN_MILLIS * 7L
+        private const val HOUR_IN_MILLIS = MINUTE_IN_MILLIS * 60L
+        private const val DAY_IN_MILLIS = HOUR_IN_MILLIS * 24L
+        private const val WEEK_IN_MILLIS = DAY_IN_MILLIS * 7L
         const val YEAR_IN_MILLIS = WEEK_IN_MILLIS * 52L
     }
 }
