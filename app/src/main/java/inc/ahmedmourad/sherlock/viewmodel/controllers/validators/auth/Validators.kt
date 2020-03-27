0895399f90a387d6abbb77141335acc45d6ca6a4
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-03-27 19:31:14 +0200

    Added more validators

--- app/src/main/java/inc/ahmedmourad/sherlock/viewmodel/controllers/validators/auth/Validators.kt
+++ app/src/main/java/inc/ahmedmourad/sherlock/viewmodel/controllers/validators/auth/Validators.kt
@@ -2,17 +2,15 @@ package inc.ahmedmourad.sherlock.viewmodel.controllers.validators.auth
 
 import arrow.core.Either
 import arrow.core.left
-import arrow.core.right
 import inc.ahmedmourad.sherlock.R
-import inc.ahmedmourad.sherlock.domain.model.auth.submodel.DisplayName
-import inc.ahmedmourad.sherlock.domain.model.auth.submodel.Email
-import inc.ahmedmourad.sherlock.domain.model.auth.submodel.PhoneNumber
+import inc.ahmedmourad.sherlock.domain.model.auth.submodel.*
 import inc.ahmedmourad.sherlock.domain.model.common.PicturePath
 import inc.ahmedmourad.sherlock.domain.model.ids.UserId
 import inc.ahmedmourad.sherlock.model.auth.AppCompletedUser
 import inc.ahmedmourad.sherlock.model.localizedMessage
 import splitties.init.appCtx
 
+//TODO: provide better messages for empty or blank
 internal fun validateEmail(value: String?): Either<String, Email> {
 
     if (value == null) {
@@ -20,6 +18,15 @@ internal fun validateEmail(value: String?): Either<String, Email> {
     }
 
     return Email.of(value).mapLeft(Email.Exception::localizedMessage)
+}
+
+internal fun validatePassword(value: String?): Either<String, Password> {
+
+    if (value == null) {
+        return appCtx.getString(R.string.malformed_email).left()
+    }
+
+    return Password.of(value).mapLeft(Password.Exception::localizedMessage)
 }
 
 internal fun validateDisplayName(value: String?): Either<String, DisplayName> {
@@ -31,8 +38,17 @@ internal fun validateDisplayName(value: String?): Either<String, DisplayName> {
     return DisplayName.of(value).mapLeft(DisplayName.Exception::localizedMessage)
 }
 
-internal fun validatePhoneNumber(location: PhoneNumber?): Either<String, PhoneNumber> {
-    return location?.right() ?: appCtx.getString(R.string.invalid_phone_number).left()
+internal fun validatePhoneNumber(countryCode: String?, number: String?): Either<String, PhoneNumber> {
+
+    if (number == null) {
+        return appCtx.getString(R.string.invalid_phone_number).left()
+    }
+
+    if (countryCode == null) {
+        return appCtx.getString(R.string.invalid_country_code).left()
+    }
+
+    return PhoneNumber.of(number, countryCode).mapLeft(PhoneNumber.Exception::localizedMessage)
 }
 
 internal fun validateAppCompletedUser(
@@ -49,4 +65,14 @@ internal fun validateAppCompletedUser(
             phoneNumber,
             picturePath
     ).mapLeft(AppCompletedUser.Exception::localizedMessage)
+}
+
+internal fun validateUserCredentials(
+        email: Email,
+        password: Password
+): Either<String, UserCredentials> {
+    return UserCredentials.of(
+            email,
+            password
+    ).mapLeft(UserCredentials.Exception::localizedMessage)
 }
